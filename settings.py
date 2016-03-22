bfc3714f9d676d87d6267f1d2d547a25c80a4ef4
Xavier Tseng <364671459@qq.com>
2016-03-22 15:04:15 +0800

    remove css update lua

---
+++ settings.py
@@ -0,0 +1,9 @@
+import sublime
+
+
+def get(key, default=None):
+    settings = sublime.load_settings('css_style_completions.sublime-settings')
+    if settings:
+        return settings.get(key, default)
+    else:
+        return default
