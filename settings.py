3bc2c49b79b6384e5511c6601fe7f8473e17fa40
XavierCHN <Xavier_CHN@live.com>
2017-11-10 15:13:50 +0800

    using modified init.lua to dump lua script completions

--- settings.py
+++
@@ -1,9 +0,0 @@
-import sublime
-
-
-def get(key, default=None):
-    settings = sublime.load_settings('css_style_completions.sublime-settings')
-    if settings:
-        return settings.get(key, default)
-    else:
-        return default
